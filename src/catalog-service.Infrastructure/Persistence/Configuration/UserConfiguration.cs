09017a7a747a59ad51623cb18b782ccca7a49ba7
douglas-reinaldo <Douglas39510@gmail.com>
2026-01-16 14:19:33 -0300

    feat(infrastructure): add Fluent API configuration for User entity

---
+++ src/catalog-service.Infrastructure/Persistence/Configuration/UserConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using catalog_service.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace catalog_service.Infrastructure.Persistence.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.FirstName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(u => u.LastName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(u => u.EmailAddress)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(u => u.HashPassword)
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+    }
+}
