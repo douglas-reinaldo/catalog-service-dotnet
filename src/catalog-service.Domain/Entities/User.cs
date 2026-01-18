bacb85206bcddc7e88705c5145368f5b854d0243
douglas-reinaldo <Douglas39510@gmail.com>
2026-01-18 07:46:30 -0300

    feat(User): add domain validation for properties

--- src/catalog-service.Domain/Entities/User.cs
+++ src/catalog-service.Domain/Entities/User.cs
@@ -1,37 +1,21 @@
 ﻿namespace catalog_service.Domain.Entities;
 
+using catalog_service.Domain.Exceptions;
 using System;
 
 public class User
 {
-    public int Id { get; private set; }
+    public int? Id { get; private set; }
     public string FirstName { get; private set; }
     public string LastName { get; private set; }
     public string EmailAddress { get; private set; }
     public string HashPassword { get; private set; }
     public bool IsActive { get; private set; }
-    public DateTime CreatedAt { get; private set; }
+    public DateTime  CreatedAt { get; private set; }
 
     public User(string firstName, string lastName, string emailAddress, string hashPassword)
 	{
-        if (string.IsNullOrWhiteSpace(firstName)) 
-        {
-            throw new ArgumentException("First name is required.", nameof(firstName));
-        }
-        if (string.IsNullOrWhiteSpace(lastName)) 
-        {
-            throw new ArgumentException("Last name is required.", nameof(lastName));
-        }
-
-        if (string.IsNullOrWhiteSpace(emailAddress)) 
-        {
-            throw new ArgumentException("Email address is required.", nameof(emailAddress));
-        }
-
-        if (string.IsNullOrWhiteSpace(hashPassword)) 
-        {
-            throw new ArgumentException("Hashed password is required.", nameof(hashPassword));
-        }
+        ValidateDomain(firstName, lastName, emailAddress, hashPassword);
 
         FirstName = firstName;
         LastName = lastName;
@@ -39,6 +23,14 @@ public class User
         HashPassword = hashPassword;
         IsActive = true;
         CreatedAt = DateTime.UtcNow;
+    }
+
+    public void ValidateDomain(string firstName, string lastName, string emailAddress, string hashPassword) 
+    {
+        DomainValidation.When(string.IsNullOrWhiteSpace(firstName), "First name is required.");
+        DomainValidation.When(string.IsNullOrWhiteSpace(lastName), "Last name is required.");
+        DomainValidation.When(string.IsNullOrWhiteSpace(emailAddress), "Email address is required.");
+        DomainValidation.When(string.IsNullOrWhiteSpace(hashPassword), "Hashed password is required.");
 
     }
 
---
+++ src/catalog-service.Domain/Exceptions/DomainValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace catalog_service.Domain.Exceptions
+{
+    public class DomainValidation : Exception
+    {
+        public DomainValidation(string message) : base(message)
+        {
+        }
+
+        public static void When(bool condition, string message) 
+        {
+            if (condition)
+            {
+                throw new DomainValidation(message);
+            }
+        }
+    }
+}
