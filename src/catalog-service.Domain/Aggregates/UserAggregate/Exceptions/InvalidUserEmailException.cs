57a418e48d4b43c2bf9e3363fb7ba3bf039abdeb
douglas-reinaldo <Douglas39510@gmail.com>
2026-01-29 13:14:37 -0300

    feat: Add Email Value Object and InvalidUserEmailException Exception

---
+++ src/catalog-service.Domain/Aggregates/UserAggregate/Exceptions/InvalidUserEmailException.cs
@@ -0,0 +1,10 @@
+﻿using catalog_service.Domain.Exceptions;
+
+namespace catalog_service.Domain.Aggregates.UserAggregate.Exceptions;
+
+public class InvalidUserEmailException : DomainException
+{
+    public InvalidUserEmailException(string message) : base(message)
+    {
+    }
+}
---
+++ src/catalog-service.Domain/Aggregates/UserAggregate/ValueObjects/Email.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Mail;
+using catalog_service.Domain.Aggregates.UserAggregate.Exceptions;
+
+namespace catalog_service.Domain.Aggregates.UserAggregate.ValueObjects;
+
+using System.Net.Mail;
+
+public sealed class Email
+{
+    public string EmailAddress { get; private set; }
+
+    private Email(string emailAddress)
+    {
+        EmailAddress = emailAddress;
+    }
+
+    public static Email Create(string emailAddress)
+    {
+        ValidateEmailNullOrWhitespace(emailAddress);
+        ValidateEmailLenght(emailAddress);
+        ValidateEmailFormat(emailAddress);
+        return new Email(emailAddress.Trim().ToLower());
+    }
+
+    private static void ValidateEmailNullOrWhitespace(string emailAddress)
+    {
+        if  (string.IsNullOrWhiteSpace(emailAddress))
+            throw new InvalidUserEmailException("Email address is required");
+    }
+    
+    private static void ValidateEmailLenght(string emailAddress)
+    {
+        if (emailAddress.Length is < 5 or > 254)
+            throw new InvalidUserEmailException("Email address must be between 5 and 254 characters");
+    }
+
+    private static void ValidateEmailFormat(string emailAddress)
+    {
+        try
+        {
+            var email = new MailAddress(emailAddress);
+        }
+        catch
+        {
+            throw new InvalidUserEmailException("Email address is not valid");
+        }
+    }
+}
