afed7cd67def665c24ae7bf386ba673a0dbcec9a
douglas-reinaldo <Douglas39510@gmail.com>
2026-01-29 12:25:46 -0300

    feat: Add Name Value Object and InvalidUserNameException exception

---
+++ src/catalog-service.Domain/Aggregates/UserAggregate/Exceptions/InvalidUserNameException.cs
@@ -0,0 +1,10 @@
+﻿using catalog_service.Domain.Exceptions;
+
+namespace catalog_service.Domain.Aggregates.UserAggregate.Exceptions;
+
+public class InvalidUserNameException : DomainException
+{
+    public InvalidUserNameException(string message) : base(message)
+    {
+    }
+}
---
+++ src/catalog-service.Domain/Aggregates/UserAggregate/ValueObjects/Name.cs
@@ -0,0 +1,43 @@
+﻿using catalog_service.Domain.Aggregates.UserAggregate.Exceptions;
+
+namespace catalog_service.Domain.Aggregates.UserAggregate.ValueObjects;
+
+public class Name
+{
+    public String FirstName { get; private set; }
+    public String LastName { get; private set; }
+
+
+    private Name(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public static Name Create(string firstName, string lastName)
+    {
+        ValidateNameNullOrWhitespace(firstName, lastName);
+        ValidateNameLength(firstName, lastName);
+        return new Name(firstName.Trim(), lastName.Trim());
+    }
+
+    private static void ValidateNameLength(string  firstName, string lastName)
+    {
+        if (firstName.Length is < 3 or > 50)
+            throw new InvalidUserNameException("Fist Name must be between 3 and 50 characters long");
+
+        if (lastName.Length is < 3 or > 50)
+            throw new InvalidUserNameException("Last Name must be between 3 and 50 characters long");
+        
+    }
+
+    private static void ValidateNameNullOrWhitespace(string firstName, string lastName)
+    {
+        if (String.IsNullOrWhiteSpace(firstName))
+            throw new InvalidUserNameException("First Name is required");
+        
+        if (String.IsNullOrWhiteSpace(lastName))
+            throw new InvalidUserNameException("Last Name is required");
+    }
+    
+}
